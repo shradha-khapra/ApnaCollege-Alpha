6279836c1454bbc91c54c51dc87c89e9498b1354
SOUVIK KOLEY <113795422+souvikkoley9674@users.noreply.github.com>
2023-01-03 01:12:38 +0530

    Update PrintInRange.java
     printLnRange fuction ,not perfectly work

--- 17_Binary Search Trees (1)/PrintInRange.java
+++ 17_Binary Search Trees (1)/PrintInRange.java
@@ -26,24 +26,17 @@ public class PrintInRange {
          return root;
     }
 
-    public static void printInRange(Node root, int k1, int k2) {
-        if(root == null) {
+     public static void printInRange(Node root, int k1, int k2) {
+        if (root == null) {
             return;
         }
-
-        if(root.data >= k1 && root.data <= k2) {
-            printInRange(root.left, k1, k2);
-            System.out.print(root.data+" ");
-            printInRange(root.right, k1, k2);
-        }
-
-        else if(root.data > k2) {
+        if (k1 < root.data) {
             printInRange(root.left, k1, k2);
         }
-
-        else {
-            printInRange(root.right, k1, k2);
+        if (root.data >= k1 && root.data <= k2) {
+            System.out.print(root.data + " ");
         }
+        printInRange(root.right, k1, k2);
     }
 
     public static void main(String args[]) {
